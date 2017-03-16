5cc5d222699065484c90aea2cd04ddff3d99eb78
fanjun <fanjun@fenbi.com>
2017-03-16 18:14:27 +0800

    add language model

--- beam.py
+++ beam.py
@@ -7,7 +7,7 @@ gram1_map = {}
 gram2_map = {}
 gram3_map = {}
 backoff2_map = {}
-
+phonemap = {}
 class Path(object):
     def __init__(self, path, prob_ctc, prob_lm, last_index):
         self.path = path
@@ -18,10 +18,10 @@ class Path(object):
     def __cmp__(self, other):
         return -1 * cmp(self.prob_ctc, other.prob_ctc)
 
-def get_lm_logprob(seq):
+def get_lm_logprob(seq, smooth_factor=-6.0):
     seq = seq[-3 : ]
-    if len(seq) < 2:
-        return 0
+    if len(seq) == 1:
+        return gram1_map[' '.join(seq)]
     tmp = ' '.join(seq)
     if tmp in gram3_map:
         return gram3_map[tmp]
@@ -30,20 +30,21 @@ def get_lm_logprob(seq):
         if tmp in backoff2_map:
             return gram2_map[tmp] + backoff2_map[tmp]
         else:
-            return 0
+            return smooth_factor
     else:
-        return 0
+        return smooth_factor
     
-def beam_search_with_lm(prob, beam_width=1000, alpha = 0.01, top_paths=1, merge_repeated=True):
-    previous_queue = Q.PriorityQueue()
-    previous_queue.put(Path(['<s>'], 0, 0, -1))
-
+def beam_search_with_lm(prob, beam_width=1000, alpha = 0.0, top_paths=1, merge_repeated=True):
+    previous_queue = Q.PriorityQueue(beam_width)
+    #previous_queue.put(Path(['40'], 0.0, 0.0, -1))
+    previous_queue.put(Path(['40'], 1.0, 1.0, -1))
+    print prob.shape
     height, width = prob.shape
     #expand frame by frame 
     for i in range(width):
         print 'step: ', i
         probi = prob[:,i]
-        topk_index = np.argsort(probi).tolist()[-20 : ] #only expand topk node
+        topk_index = np.argsort(probi)[-1 * 20 : ].tolist() #only expand topk node
         topk_index.reverse() #prob decrease
 
         select_queue = Q.PriorityQueue()
@@ -51,16 +52,20 @@ def beam_search_with_lm(prob, beam_width=1000, alpha = 0.01, top_paths=1, merge_
         while not previous_queue.empty():
             path = previous_queue.get()
             for index in topk_index:
-                probK = log(probi[index], 10)
+                #probK = log(probi[index],10)
+                probK = probi[index]
                 path_new = path.path[:]
                 path_new.append(str(index))
-                prob_ctc = path.prob_ctc +  probK
-                prob_lm = get_lm_logprob(path_new) +  path.prob_lm
+                #prob_ctc = path.prob_ctc +  probK
+                prob_ctc = path.prob_ctc * probK
+                prob_lm = pow(10, get_lm_logprob(path_new)) * path.prob_lm
+                #print 'prob_lm = %f, prob_ctc = %f' % (log(prob_lm, 10), log(prob_ctc, 10))
                 prob_ctc = prob_ctc + alpha * prob_lm
+                #print path_new, pow(10, prob_ctc)
                 select_queue.put(Path(path_new, prob_ctc, prob_lm, index))
-        
+            #print '+++++++++++++' 
         cnt = 0
-        current_queue = Q.PriorityQueue()
+        current_queue = Q.PriorityQueue(beam_width)
         while not select_queue.empty() and cnt < beam_width:
             path = select_queue.get()
             current_queue.put(path)
@@ -70,6 +75,7 @@ def beam_search_with_lm(prob, beam_width=1000, alpha = 0.01, top_paths=1, merge_
     result = {}
     while not previous_queue.empty():
         path = previous_queue.get()
+        #print path.path
         merge_path = joint(path.path) #delete duplicate phone
         if merge_path in result: #merge same paths
             result[merge_path] += path.prob_ctc
@@ -77,7 +83,7 @@ def beam_search_with_lm(prob, beam_width=1000, alpha = 0.01, top_paths=1, merge_
             result[merge_path] = path.prob_ctc
     
     sortresult = sorted(result.items(), key = lambda x : x[1], reverse = True)
-    print sortresult[0]    
+    print sortresult[0:10]    
 
 def joint(phonelist):
     if phonelist is None or len(phonelist) == 0 :
@@ -90,7 +96,7 @@ def joint(phonelist):
         else:
             templist.append(phone)
             lastphone = phone
-    return ' '.join(templist) 
+    return ' '.join(templist).replace(' 39','') 
 
 def build_prob(inpath):
     reader = open(inpath, 'r')
@@ -102,16 +108,26 @@ def build_prob(inpath):
         frames.append(nums)
     return np.array(frames)
 
+def loadphonemap(inpath):
+    phonemap = {}
+    with open(inpath, 'r') as reader:
+        for line in reader:
+            phone, ind = line.strip().split('\t')
+            phonemap[ind] = phone
+    return phonemap
+
+def translate(ind_list):
+    print '_'.join(ind_list)
 
 if __name__ == '__main__':
     inpath = '34.prob.txt'
     lm_path = 'lls_libris_o3.txt'
-    global gram1_map
-    global gram2_map
-    global gram3_map
-    global backoff2_map
-
+    #global gram1_map
+    #global gram2_map
+    #global gram3_map
+    #global backoff2_map
+    #global phonemap
+    phonemap = loadphonemap('phonemap.txt')
     prob = build_prob(inpath)    
     gram1_map, gram2_map, gram3_map, backoff2_map = load_model(lm_path)
-    print prob.shape
-    beam_search_with_lm(prob.transpose(), beam_width = 100, alpha = 0.0) 
+    beam_search_with_lm(prob.transpose(), beam_width = 1000, alpha = 0.0) 
